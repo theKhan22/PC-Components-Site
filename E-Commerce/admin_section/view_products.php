e1bfe636080f478bc43f6e55b999a61f66b38f1a
Alif Khan <100114001+theKhan22@users.noreply.github.com>
2022-12-13 22:03:44 +0600

    Add files via upload

---
+++ E-Commerce/admin_section/view_products.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3 class="text-center text-success">All Products</h3>
+    <table class='table table-bordered mt-5'>
+        <thead class='bg-info'>
+            <tr>
+                <th>Product ID</th>
+                <th>Product Title</th>
+                <th>Product Image</th>
+                <th>Product Price</th>
+                <th>Total Sold</th>
+                <th>Status</th>
+                <th>Edit</th>
+                <th>Delete</th>
+
+            </tr>
+
+        </thead>
+
+        <tbody class='bg-secondary text-light'>
+
+        <tr class='text-center'>
+            <td>1</td>
+            <td>Mango</td>
+            <td>Image</td>
+            <td>44</td>
+            <td>2</td>
+            <td>true</td>
+            <td><a href='' class='text-light'><i class='fa-solid fa-pen-to-square'></i></a></td>
+            <td><a href='' class='text-light'><i class='fa-solid fa-trash'></i></a></td>
+        </tr>
+
+
+        </tbody>
+
+    </table>
+</body>
+</html>
