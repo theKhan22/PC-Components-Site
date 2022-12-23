1de3bd1e75b2c4a6e40191ac5915c676864a43e2
Alif Khan <100114001+theKhan22@users.noreply.github.com>
2022-12-23 22:21:45 +0600

    Add files via upload

--- tech_store.sql
+++ tech_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2022 at 04:12 PM
+-- Generation Time: Dec 23, 2022 at 05:03 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `tech_store`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_table`
+--
+
+CREATE TABLE `admin_table` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(100) NOT NULL,
+  `admin_email` varchar(100) NOT NULL,
+  `admin_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_table`
+--
+
+INSERT INTO `admin_table` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
+(3, 'Alif', 'alif@gmail.com', '123');
 
 -- --------------------------------------------------------
 
@@ -55,14 +75,6 @@ CREATE TABLE `cart_details` (
   `quantity` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `cart_details`
---
-
-INSERT INTO `cart_details` (`product_id`, `ip_address`, `quantity`) VALUES
-(1, '::1', 0),
-(2, '::1', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -80,7 +92,8 @@ CREATE TABLE `categories` (
 
 INSERT INTO `categories` (`category_id`, `category_title`) VALUES
 (1, 'Monitor'),
-(2, 'Mouse');
+(2, 'Mouse'),
+(3, 'Keyboard');
 
 -- --------------------------------------------------------
 
@@ -102,13 +115,9 @@ CREATE TABLE `orders_pending` (
 --
 
 INSERT INTO `orders_pending` (`order_id`, `user_id`, `invoice_number`, `product_id`, `quantity`, `order_status`) VALUES
-(1, 1, 732508090, 2, 1, 'pending'),
-(2, 1, 1623086020, 2, 1, 'pending'),
-(3, 1, 224998329, 2, 1, 'pending'),
-(4, 1, 749301320, 2, 1, 'pending'),
-(5, 1, 451298246, 2, 1, 'pending'),
-(6, 1, 1886045964, 2, 1, 'pending'),
-(7, 1, 377506670, 2, 1, 'pending');
+(1, 7, 31838689, 23, 3, 'pending'),
+(2, 1, 128001442, 24, 1, 'pending'),
+(3, 1, 876788598, 25, 1, 'pending');
 
 -- --------------------------------------------------------
 
@@ -136,8 +145,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_title`, `product_description`, `product_keywords`, `category_id`, `brand_id`, `product_image1`, `product_image2`, `product_image3`, `product_price`, `date`, `status`) VALUES
-(1, 'LG 19M38A', 'Model: LG 19M38A Resolution: 1366 x 768 Display: TN, 60Hz, 5ms Ports: VGA Features: Flicker Free', 'monitor', 1, 1, 'LG 19M38A-500x500.jpg', 'LG 19M38A-500x500.jpg', 'LG 19M38A-500x500.jpg', '10', '2022-12-02 07:04:56', 'true'),
-(2, 'HP V19', 'MPN: 9TN42AA Model: V19 Resolution: WXGA (1366 x 768) Display: TN, 60Hz, 5ms Ports: VGA Features: Low Blue Light', 'monitor', 1, 2, 'v19-03-500x500.jpg', 'v19-03-500x500.jpg', 'v19-03-500x500.jpg', '20', '2022-12-02 07:05:09', 'true');
+(1, 'LG 19M38A', 'Model: LG 19M38A Resolution: 1366 x 768 Display: TN, 60Hz, 5ms Ports: VGA Features: Flicker Free', 'monitor,lg', 0, 0, 'LG 19M38A-500x500.jpg', 'LG 19M38A-500x500.jpg', 'samsung back.png', '10', '2022-12-23 14:51:10', 'true'),
+(20, 'Samsung V206HQL Abi 19.5\" HD Monitor', 'MPN: UM.IV6ST.A01 Model: V206HQL Abi Resolution: HD (1600 x 900) Display: TN, 60Hz, 5ms Ports: VGA, HDMI Features: Ergonomic design', 'monitor,MONITOR,Monitor', 1, 4, 'samsung.png', 'samsung back.png', 'samsung left.png', '9000', '2022-12-22 14:37:06', 'true'),
+(21, 'LG M170 Wireless Mouse', 'Model: Logitech M170 Connection Type: 2.4 Ghz wireless Wireless range: 10 m (33 ft) Battery life: 1 year', 'mouse,MOUSE,Mouse', 2, 1, 'm170-500x500.png', 'm170-500x500.png', 'm170-500x500.png', '1099', '2022-12-22 14:45:01', 'true'),
+(22, 'Havit N100 Wireless Mouse', 'MPN: 888015276 Model: Lenovo N100 Type Wireless Max Operating Humidity 95% Weight 0.061Kg', 'mouse,Mouse', 2, 3, 'n100-500x500.jpg', 'n100-500x500.jpg', 'n100-500x500.jpg', '1100', '2022-12-22 14:45:46', 'true'),
+(23, 'Havit MS851 USB Mouse', 'Model: MS851 Resolution: 1000DPI & Key Number: 3 3D Gyro Wheel Function Operating Voltage: 5V Key Life: 3,000,000 times', 'mouse,havit,Mouse,MOUSE', 2, 3, 'ms851-1-500x500.jpg', 'ms851-1-500x500.jpg', 'ms851-1-500x500.jpg', '230', '2022-12-22 14:48:14', 'true'),
+(24, 'HP U2000 Keyboard & Mouse Wired Combo', 'MPN: 90-XB1000KM000F0 Model: U2000 Elegant style, solid construction Built-in multimedia hot keys Driver-less plug and play Resolution: 1000dpi', 'keyboard,KEYBOARD', 3, 2, 'u2000-000-500x500.jpg', 'u2000-002-500x500.jpg', 'u2000-000-500x500.jpg', '1399', '2022-12-22 14:50:33', 'true'),
+(25, 'LG 22MP400-B 22-inch Full HD Monitor', 'Model: 22MP400-B Resolution: FHD (1920 x 1080) Display: VA, 75Hz, 5ms (GtG at Faster) Ports: 1x HDMI, 1x D-Sub Features: AMD Free Sync, Smart Energy Saving, Flicker Free', 'monitor,lg', 1, 1, 'lg-22mp400-b-01-500x500.jpg', 'lg-22mp400-b-01-500x500.jpg', 'lg-22mp400-b-01-500x500.jpg', '13200', '2022-12-23 15:58:10', 'true');
 
 -- --------------------------------------------------------
 
@@ -160,9 +174,32 @@ CREATE TABLE `user_orders` (
 --
 
 INSERT INTO `user_orders` (`order_id`, `user_id`, `amount_due`, `invoice_number`, `total_products`, `order_date`, `order_status`) VALUES
-(7, 1, 30, 451298246, 2, '2022-12-11 04:31:11', 'pending'),
-(8, 1, 20, 1886045964, 1, '2022-12-11 04:50:18', 'pending'),
-(9, 1, 30, 377506670, 2, '2022-12-11 14:50:03', 'pending');
+(1, 7, 720, 31838689, 2, '2022-12-23 15:45:51', 'Complete'),
+(2, 1, 2508, 128001442, 3, '2022-12-23 15:59:49', 'Complete'),
+(3, 1, 13430, 876788598, 2, '2022-12-23 16:00:18', 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_payments`
+--
+
+CREATE TABLE `user_payments` (
+  `payment_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `invoice_number` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `payment_mode` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_payments`
+--
+
+INSERT INTO `user_payments` (`payment_id`, `order_id`, `invoice_number`, `amount`, `payment_mode`, `date`) VALUES
+(1, 1, 31838689, 720, 'Cash On Delivery', '2022-12-23 15:45:51'),
+(2, 2, 128001442, 2508, 'Cash On Delivery', '2022-12-23 15:59:49');
 
 -- --------------------------------------------------------
 
@@ -186,14 +223,18 @@ CREATE TABLE `user_table` (
 --
 
 INSERT INTO `user_table` (`user_id`, `username`, `user_email`, `user_password`, `user_image`, `user_ip`, `user_address`, `user_mobile`) VALUES
-(1, 'Alifa', 'alif@gmail.com', '123', 'logo.png', '::1', 'dhaka', '1234'),
-(3, 'Khanam', '123@gma', '123', '', '::1', '123', '1234'),
-(4, 'seema', 'seema@gmail.com', '123', 'v19-03-500x500.jpg', '::1', '123', '1234'),
-(5, 'Ka', 'aa@ff', '123', 'logo.png', '::1', '11', '11');
+(1, 'Khan', 'alif@yahoo.com', '123', 'IMG_20211128_000716_401.jpg', '::1', 'Dhaka', '1122'),
+(7, 'Alif', 'alif@gmail.com', '123', 'IMG_20211128_000716_401.jpg', '::1', 'Dhaka', '123');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin_table`
+--
+ALTER TABLE `admin_table`
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `brands`
@@ -232,6 +273,12 @@ ALTER TABLE `user_orders`
   ADD PRIMARY KEY (`order_id`);
 
 --
+-- Indexes for table `user_payments`
+--
+ALTER TABLE `user_payments`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
 -- Indexes for table `user_table`
 --
 ALTER TABLE `user_table`
@@ -240,6 +287,12 @@ ALTER TABLE `user_table`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin_table`
+--
+ALTER TABLE `admin_table`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `brands`
@@ -251,31 +304,37 @@ ALTER TABLE `brands`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `orders_pending`
 --
 ALTER TABLE `orders_pending`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `user_orders`
 --
 ALTER TABLE `user_orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_payments`
+--
+ALTER TABLE `user_payments`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_table`
 --
 ALTER TABLE `user_table`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
